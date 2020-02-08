272aecd21b65606c71a88197fbe915c04dc8fd55
Thanut Pheeraphan <55307470+thanutpheeraphan@users.noreply.github.com>
2020-02-08 23:53:14 +0700

    Add files via upload

--- internship_3.py
+++ internship_3.py
@@ -1,26 +1,27 @@
+## Question 3
 inp = (input("Put in 12 numbers: "))
-#print(inp)
-temp = inp.split()
-lst2=[]
-for i in range(len(temp)):
-    lst2.append('_')
+inp_list = inp.split()
+ans_list = ['_'] * 12
+
+for _ in range (12):
     print("_ ", end='')
+    
 count = 5
 score = 0
-print("\n")
+print()
+
 while (count != 0):
     inp2 = (input("Your guess: "))
-
-    count -=1
-    if (inp2 in temp):
-        for i in range (len(temp)):
-            if(temp[i] == inp2):
+    count -= 1
+    
+    if (inp2 in inp_list):
+        for i in range (len(inp_list)):
+            if(inp_list[i] == inp2):
                 score +=1
-                lst2[i] = temp[i]
+                ans_list[i] = inp_list[i]
+    elif inp2 not in ans_list:
+        ans_list.append(inp2)
 
-        print(' '.join(lst2))
-    else:
-        lst2.append(inp2)
-        print(' '.join(lst2))
+    print(' '.join(ans_list))
 
 print("Score: ",score)
