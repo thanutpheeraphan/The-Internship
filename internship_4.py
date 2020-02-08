9890e825145dccc0fc87dcb4c3c2d53e4043aa34
Thanut Pheeraphan <55307470+thanutpheeraphan@users.noreply.github.com>
2020-02-08 23:51:06 +0700

    Add files via upload

--- internship_4.py
+++ internship_4.py
@@ -4,15 +4,15 @@ from flask import request, jsonify
 from bs4 import BeautifulSoup
 
 ## Question 4.1
-start_url = requests.get('https://theinternship.io/')
-soup = BeautifulSoup(start_url.text, 'html.parser')
-image_tags = soup.findAll('img',class_ ='center-logos')
-description_tags = soup.findAll('span',class_ ='list-company')
+start_url = requests.get("https://theinternship.io/")
+soup = BeautifulSoup(start_url.text, "html.parser")
+image_tags = soup.findAll("img",class_ ="center-logos")
+description_tags = soup.findAll("span",class_ ="list-company")
 companies = {}
 companies_api = []
 final_companies_api = []
 for i in range (len(image_tags)):
-    companies[image_tags[i].get('src')] = len(description_tags[i].get_text())
+    companies[image_tags[i].get("src")] = len(description_tags[i].get_text())
 
 companies = {key: value for key, value in sorted(companies.items(), key=lambda item: item[1])}
 
@@ -21,15 +21,15 @@ for image_source in companies.keys():
 
 ## Question 4.2
 for image_source in companies.keys():
-    final_image_source = 'https://theinternship.io/' + image_source
-    companies_api.append({'logo' : final_image_source})
+    final_image_source = "https://theinternship.io/" + image_source
+    companies_api.append({"logo" : final_image_source})
 
-final_companies_api = {'companies' : companies_api}
+final_companies_api = {"companies" : companies_api}
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/companies', methods=['GET'])
+@app.route("/companies", methods=["GET"])
 
 def gen_api():
     return jsonify(final_companies_api)
